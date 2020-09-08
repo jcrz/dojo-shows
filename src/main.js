a13e7063f6ed6a528ea9adade76c006d201fe58a
jcrz <jfuentes.dev@gmail.com>
2020-09-08 03:13:55 -0300

    configuracion inicial

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueFirestore from 'vue-firestore';       // adding this import
 
 Vue.config.productionTip = false
+
+Vue.use(VueFirestore, {key: 'id'});      // adding this use statement
 
 new Vue({
   render: h => h(App),
